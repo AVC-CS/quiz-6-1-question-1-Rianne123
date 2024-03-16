7a639d9161d792c760beb7a4e7d767eec478b5ff
Rianne123 <156950124+Rianne123@users.noreply.github.com>
2024-03-16 05:59:05 +0000

    implemented getPrevPrime function

--- main.hpp
+++ main.hpp
@@ -1,32 +1,44 @@
 #include <iostream>
 using namespace std;
-void getTwoValues(int, int);
+int getTwoValues(int, int);
 int getNextPrime(int);
 int getPrevPrime(int);
     
-void getTwoValues (int begin, int end) {
+int begin=0, end=0;
+int prime1, prime2;
+int getTwoValues (int begin, int end) {
     do {
     cout << "Enter two integers: " << endl;  
     cin >> begin >> end;
     } while (begin > end);
 }
 
-int getNextPrime (int begin) {
-    for (int i = begin; i < i+1; i++) {
-		int prime=1;
-		for (int j = 2; j < (i/2) + 1; j++) {
-			if (i % j == 0) {
-				prime = 0;
-				break;
-			}	
-		}
-		if (prime) { 
-				cout << i << " is the next Prime Number" << endl;
+int getNextPrime(int begin) {
+    for (int i = begin + 1;; i++) { // Start searching from begin + 1
+        int prime = 1;
+        for (int j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = 0;
                 break;
-		}
-	}
+            }
+        }
+        if (prime) {
+            return i;
+        }
+    }
 }
 
-int getPrevPrime (int end) {
-
+int getPrevPrime(int end) {
+    for (int i = end - 1;; i--) { // Start searching from end - 1
+        int prime = 1;
+        for (int j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+        if (prime) {
+            return i;
+        }
+    }
 }
